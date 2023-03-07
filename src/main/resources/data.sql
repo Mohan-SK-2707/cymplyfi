c09d20ed53f243788db5b0ccb31c59e389ab1bd7
BackendDev2021 <mohanmohanlal1998@gmail.com>
2023-03-07 14:47:21 +0530

    static data queries updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,16 +15,16 @@ INSERT INTO employee_department (id, department_name, manager_Id) VALUES (7, 'In
 
 
 --Queries to generate static data in the employee table--
-INSERT INTO employee_personal_details (id,first_name,last_name,official_email,role) VALUES (1,"Ali","Afroze","afroze@flyerssoft.com","ADMIN");
+INSERT INTO employee_personal_details (id,first_name,last_name,email,official_email,employee_gender,contact_number,designation,role,department_id) VALUES (1,"Ali","Afroze","afroze@gmail.com","afroze@flyerssoft.com","MALE","9003218899","MANAGER","ADMIN",1);
 
-INSERT INTO employee_personal_details (id,first_name,last_name,official_email,role) VALUES (2,"Chakravarthy","chowdry","mourya@flyerssoft.com","ADMIN");
+INSERT INTO employee_personal_details (id,first_name,last_name,email,official_email,employee_gender,contact_number,designation,role,department_id) VALUES (2,"Chakravarthy","chowdry","mourya@flyerssoft.com","mourya@flyerssoft.com","MALE","9003218899","MANAGER","ADMIN",2);
 
-INSERT INTO employee_personal_details (id,first_name,last_name,official_email,role) VALUES (3,"Abhinav","abhinav","abhinav@flyerssoft.com","ADMIN");
+INSERT INTO employee_personal_details (id,first_name,last_name,email,official_email,employee_gender,contact_number,role,designation,department_id) VALUES (3,"Abhinav","abhinav","abhinav@gmail.com","abhinav@flyerssoft.com","MALE","9003218899","MANAGER","ADMIN",3);
 
-INSERT INTO employee_personal_details (id,first_name,last_name,official_email,role) VALUES (4,"Subhash","subhash","subhash@flyerssoft.com","ADMIN");
+INSERT INTO employee_personal_details (id,first_name,last_name,email,official_email,employee_gender,contact_number,role,designation,department_id) VALUES (4,"Subhash","subhash","subhash@gmail.com","subhash@flyerssoft.com","MALE","9003218899","MANAGER","ADMIN",4);
 
-INSERT INTO employee_personal_details (id,first_name,last_name,official_email,role) VALUES (5,"Vignesh","sekhar","vsekhar@flyerssoft.com","ADMIN");
+INSERT INTO employee_personal_details (id,first_name,last_name,email,official_email,employee_gender,contact_number,role,designation,department_id) VALUES (5,"Vignesh","sekhar","vsekhar@gmail.com","vsekhar@flyerssoft.com","MALE","9003218899","MANAGER","ADMIN",5);
 
-INSERT INTO employee_personal_details (id,first_name,last_name,official_email,role) VALUES (6,"Ananthu","ananthu","ananthu@flyerssoft.com","ADMIN");
+INSERT INTO employee_personal_details (id,first_name,last_name,email,official_email,employee_gender,contact_number,role,designation,department_id) VALUES (6,"Ananthu","ananthu","ananthu@gmail.com","ananthu@flyerssoft.com","MALE","9003218899","MANAGER","ADMIN",6);
 
-INSERT INTO employee_personal_details (id,first_name,last_name,official_email,role) VALUES (7,"Arivan","ramesh","aramesh@flyerssoft.com","ADMIN");
+INSERT INTO employee_personal_details (id,first_name,last_name,email,official_email,employee_gender,contact_number,designation,role,department_id) VALUES (7,"Arivan","ramesh","aramesh@gmail.com","aramesh@flyerssoft.com","MALE","9003218899","MANAGER","ADMIN",7);
