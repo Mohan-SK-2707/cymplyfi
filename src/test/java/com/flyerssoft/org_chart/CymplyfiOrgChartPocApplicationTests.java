7b8d3f0f36161cc282a9f625f39f6c8d731ca5f1
abineshprabhakaran <abineshprabhakaran97@gmail.com>
2023-03-08 13:45:53 +0530

    Docker update

--- src/test/java/com/flyerssoft/org_chart/CymplyfiOrgChartPocApplicationTests.java
+++ src/test/java/com/flyerssoft/org_chart/CymplyfiOrgChartPocApplicationTests.java
@@ -1,13 +1,13 @@
-package com.flyerssoft.org_chart;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CymplyfiOrgChartPocApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.flyerssoft.org_chart;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+////class CymplyfiOrgChartPocApplicationTests {
+////
+////	@Test
+////	void contextLoads() {
+////	}
+////
+////}
